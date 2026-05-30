34abb531d36ed9d76fce20ac95321da876c328cd
vikasyaduvanshi <vikasyaduvanshi>
2026-05-30 14:59:42 +0000

    tegu: bring up AlphaDroid
    Signed-off-by: vikasyaduvanshi <vikasyaduvanshi>

--- alpha_tegu.mk
+++ alpha_tegu.mk
@@ -5,7 +5,9 @@
 #
 
 # Inherit some common stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
 
 # Inherit device configuration
 DEVICE_CODENAME := tegu
@@ -16,11 +18,32 @@ $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9a
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2424
 TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_HOTWORD_BUILD := true
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build Config
+TARGET_BUILD_PACKAGE := 3
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Vikas(Optimus)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tegu-user 16 BP4A.260205.001 14624666 release-keys" \
